34f7f14e9f7800b0fca154093efd8bbb1faf9c8d
dudongxu <dudongxu@meituan.com>
2026-02-10 10:18:32 +0800

    vulkano update rust bulid script

--- rust/vulkano_lab/ndk_build.sh
+++ rust/vulkano_lab/ndk_build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env zsh
-
 set -eu
 
-# shellcheck disable=SC2006
-BIN_DIR=`dirname "$0"`
-ANDROID_DIR="${BIN_DIR}/../../Android"
-ANDROID_DIR=`realpath "$ANDROID_DIR"`
+TARGETS=("aarch64-linux-android" "armv7-linux-androideabi" "x86_64-linux-android")
+OUTPUT_DIR="../../Android/app/src/main/jniLibs/"
+BIN_DIR=$(dirname "$0")
+ANDROID_DIR="${BIN_DIR}/../../android"
+ANDROID_DIR=$(realpath "$ANDROID_DIR")
 GRADLE_PROPERTIES_FILE="${ANDROID_DIR}/gradle.properties"
-
 read_properties() {
   file="$1"
   key="$2"
@@ -19,12 +18,15 @@ read_properties() {
   value=$(echo "$value" | awk -F= '{print $2}' | tr -d ' ')
   echo "$value"
 }
-ANDROID_BUILD_NDK_VERSION=25.1.8937393
+
+ANDROID_BUILD_NDK_VERSION=$(read_properties "$GRADLE_PROPERTIES_FILE" "ANDROID_BUILD_NDK_VERSION")
 ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
 export ANDROID_NDK_HOME="$ANDROID_SDK_ROOT/ndk/$ANDROID_BUILD_NDK_VERSION"
 if [ ! -d "$ANDROID_NDK_HOME" ]; then
   echo "Directory '$ANDROID_NDK_HOME' does not exist or is not a directory."
   exit 1
 fi
-
-cargo ndk --platform 21 --target armv7-linux-androideabi --output-dir ../../Android/app/src/main/jniLibs/ --no-strip build
+for target in "${TARGETS[@]}"; do
+    echo "Building for target: $target"
+    cargo ndk --platform 21 --target "$target" --output-dir "$OUTPUT_DIR" --no-strip build
+done
